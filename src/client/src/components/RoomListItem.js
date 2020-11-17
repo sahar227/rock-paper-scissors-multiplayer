248f8d3a2721fd1ffbdf6d86177f605a58029304
sahar227 <sahar22798@gmail.com>
2020-11-17 10:37:59 +0200

    improved room item visibility on mobile

--- src/client/src/components/RoomListItem.js
+++ src/client/src/components/RoomListItem.js
@@ -4,7 +4,7 @@ export default function RoomListItem({roomData, joinRoom}) {
     const [password, setPassword] = useState('');
     const passwordPlaceHolder = roomData.isPasswordProtected ? "Enter room password" : "No password required";
     return (
-        <li style={{display:"flex", justifyContent:"space-around", width:"30%", backgroundColor:"grey"}}>
+        <li style={{display:"flex", justifyContent:"space-around", backgroundColor:"grey"}}>
             <p>{roomData.roomName}</p>
             <input type="text" value={password} onChange={e => setPassword(e.target.value)} disabled={!roomData.isPasswordProtected} placeholder={passwordPlaceHolder}/>
             <button style={{padding:"0 2rem", backgroundColor:"white"}} onClick={() => joinRoom(password)}>Join</button>
